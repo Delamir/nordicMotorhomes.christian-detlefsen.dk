3e861a92705fb372870573b14c916ea94fa7b3cb
Kringelum2 <p.kringelum@gmail.com>
2021-05-12 10:43:13 +0200

    Initial commit

---
+++ src/main/java/grp1/motorhomes/MotorhomesApplication.java
@@ -0,0 +1,13 @@
+package grp1.motorhomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorhomesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotorhomesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/grp1/motorhomes/MotorhomesApplicationTests.java
@@ -0,0 +1,13 @@
+package grp1.motorhomes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorhomesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
