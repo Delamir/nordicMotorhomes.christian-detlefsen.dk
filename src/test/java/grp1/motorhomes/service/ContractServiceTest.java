731736beaf5febd9922a469e9f9f2baf03b53d83
Delamir <71318139+Delamir@users.noreply.github.com>
2021-05-20 10:20:23 +0200

    ContractServiceTest made

---
+++ src/test/java/grp1/motorhomes/service/ContractServiceTest.java
@@ -0,0 +1,16 @@
+package grp1.motorhomes.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContractServiceTest {
+
+    @Test
+    void calculatePrice() {
+    }
+
+    @Test
+    void cancellationFee() {
+    }
+}
