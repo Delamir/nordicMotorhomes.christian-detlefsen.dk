ddd19db571e8ee285f415d34fcab13de16b9a3fd
Delamir <71318139+Delamir@users.noreply.github.com>
2021-05-24 11:06:58 +0200

    fuck dig patrick

--- src/main/java/grp1/motorhomes/model/AutoService.java
+++ src/main/java/grp1/motorhomes/model/AutoService.java
@@ -1,6 +1,5 @@
 package grp1.motorhomes.model;
 
-import grp1.motorhomes.repository.MotorhomeRepo;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -13,14 +12,58 @@ import javax.persistence.OneToOne;
 public class AutoService {
     @Id
     private int autoServiceId;
+
     private String autocheck;
     private boolean checked;
-
     @OneToOne
-    private Motorhome;
+    private Motorhome motorhome;
 
     /**
      * @author Christian
      */
-    public
+    public AutoService() {
+
+    }
+
+    /**
+     * @author Christian
+     */
+    public AutoService(int autoServiceId, String autocheck, boolean checked, Motorhome motorhome) {
+        this.autoServiceId = autoServiceId;
+        this.autocheck = autocheck;
+        this.checked = checked;
+        this.motorhome = motorhome;
+    }
+
+    public int getAutoServiceId() {
+        return autoServiceId;
+    }
+
+    public void setAutoServiceId(int autoServiceId) {
+        this.autoServiceId = autoServiceId;
+    }
+
+    public String getAutocheck() {
+        return autocheck;
+    }
+
+    public void setAutocheck(String autocheck) {
+        this.autocheck = autocheck;
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+
+    public Motorhome getMotorhome() {
+        return motorhome;
+    }
+
+    public void setMotorhome(String licencePlate, String type, String brand, String model, String description, String imagePath, int price, boolean available) {
+        this.motorhome = new Motorhome(licencePlate, type, brand, model, description, imagePath, price, available);
+    }
 }
---
+++ src/main/java/grp1/motorhomes/repository/AutoServiceResultSetExtractor.java
@@ -0,0 +1,71 @@
+package grp1.motorhomes.repository;
+
+import grp1.motorhomes.model.Contract;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ResultSetExtractor;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class AutoServiceResultSetExtractor implements ResultSetExtractor {
+
+    /**
+     * the method that is run when the class tries to map data from a resultSet from the JdbcTemplate
+     * @param resultSet
+     * @return
+     * @throws SQLException
+     * @throws DataAccessException
+     */
+    @Override
+    public List<Contract> extractData(ResultSet resultSet) throws SQLException, DataAccessException {
+
+        // we use hash map to make sure there is only one object of a contract with a given contract id
+        HashMap<Integer, Contract> contracts = new HashMap<>();
+
+        // we run trough all the rows of the query
+        while(resultSet.next()){
+
+            // we see if the contract is already in the hash map
+            Contract contract = contracts.get(resultSet.getInt("contract_id"));
+
+            // if it is not we make a new contract object and put it in the map
+            if(contract == null){
+
+                contract = new Contract();
+                contract.setContractId(resultSet.getInt("contract_id"));
+                contract.setFromDate(resultSet.getTimestamp("from_Date").toString());
+                contract.setToDate(resultSet.getTimestamp("to_Date").toString());
+                contract.setOdometer(resultSet.getInt("odometer"));
+                contract.setCustomer(resultSet.getInt("customer_number"),
+                        resultSet.getString("name"), resultSet.getString("licence_number"),
+                        resultSet.getString("street"), resultSet.getString("city"), resultSet.getInt("post_code"));
+                contract.setMotorhome(resultSet.getString("motorhome"),
+                        resultSet.getString("type"), resultSet.getString("brand"),
+                        resultSet.getString("model"), resultSet.getString("description"),
+                        resultSet.getString("image_path"), resultSet.getInt("price"), resultSet.getBoolean("available"));
+                contract.setExcessKm(resultSet.getInt("excess_km"));
+                contract.setTransferKm(resultSet.getInt("transfer_km"));
+                contract.setDeliveryPoint(resultSet.getString("delivery_point"));
+                contract.setDelivered(resultSet.getBoolean("delivered"));
+                contract.setPickupPoint(resultSet.getString("pickup_point"));
+                contract.setPickedUp(resultSet.getBoolean("picked_up"));
+                contract.setClosed(resultSet.getBoolean("closed"));
+
+                contracts.put(contract.getContractId(),contract);
+
+            }
+
+            // lastly we add the extra of the current row of the result set
+            contract.addExtra(resultSet.getInt("extra_id"), resultSet.getInt("price"),
+                    resultSet.getString("name"), resultSet.getString("description"));
+
+        }
+
+        return new ArrayList<>(contracts.values());
+    }
+}
+
